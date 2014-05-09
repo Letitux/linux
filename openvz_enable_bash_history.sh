39f4a51a11eb452a1ec436cc927c10d9762457f8
Letitux <eric.valois@gmail.com>
2014-05-09 07:10:47 -0400

    Update openvz_enable_bash_history.sh

--- openvz_enable_bash_history.sh
+++ openvz_enable_bash_history.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # This script enable bash history that is disabled by default on openvz container.
-
+# Usage: wget https://raw.githubusercontent.com/Letitux/linux/master/openvz_enable_bash_history.sh -O /tmp/openvz_enable_bash_history.sh; sh /tmp/openvz_enable_bash_history.sh
 
 echo 'Enabling bash history if not already enabled ...'
 
